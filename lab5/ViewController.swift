2a6ca9f642cd9cb7a24b5254e5c84227f1379114
romander333 <romander09@gmail.com>
2025-11-17 22:53:01 -0800

    Initial Commit

---
+++ lab5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab5
+//
+//  Created by ІПЗ-31/2 on 17.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
